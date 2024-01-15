f07db174355c86e2a0190be7d50c3956758e7a87
tolku <91892836+tolku@users.noreply.github.com>
2024-01-15 22:56:56 +0100

    project ini files

---
+++ src/main/java/com/fodapi/configuration/FodapiApplication.java
@@ -0,0 +1,14 @@
+package com.fodapi.configuration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication (exclude = {DataSourceAutoConfiguration.class }, scanBasePackages = {"com.fodapi"})
+public class FodapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FodapiApplication.class, args);
+	}
+
+}

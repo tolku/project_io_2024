77a2b2ee487e2888ac1f370ccad956466f614d44
Małek Michał <malekstudia@gmail.com>
2024-01-22 19:16:21 +0100

    Obliczanie diety

--- src/main/java/com/fodapi/controllers/DietController.java
+++ src/main/java/com/fodapi/controllers/DietController.java
@@ -1,9 +1,13 @@
 package com.fodapi.controllers;
 
 import com.fodapi.components.DietComponent;
+import com.fodapi.components.UserComponent;
 import com.fodapi.models.diets.DietEntity;
+import com.fodapi.models.user.Gender;
+import com.fodapi.models.user.UserEntity;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class DietController {
 
     private final DietComponent dietComponent;
+    private final UserComponent userComponent;
     private final DietEntity emptyDiet;
 
     @GetMapping("/diet")
@@ -21,4 +26,30 @@ public class DietController {
 
         return dietByUserId;
     }
+
+    @GetMapping("/addDiet")
+    public void addDiet(/*@AuthenticationPrincipal UserDetails userDetails*/ @RequestParam Double activityLevel,
+                                                                             @RequestParam Integer age,
+                                                                             @RequestParam Gender gender,
+                                                                             @RequestParam Double height,
+                                                                             @RequestParam Double weight){
+        UserEntity user = userComponent.getUser(1L);
+        Long userId = user.getId();
+        user.setAge(age);
+        user.setGender(gender);
+        user.setHeight(height);
+        user.setWeight(weight);
+        userComponent.saveUser(user);
+
+        if(age==null || gender==null || height==null || weight==null || activityLevel==null){
+            DietEntity dietEmp = emptyDiet.clone();
+            dietEmp.setId(null);
+            dietEmp.setUserId(userId);
+            dietComponent.addDiet(emptyDiet);
+        }else {
+            DietEntity diet = dietComponent.computeDiet(userId,age,gender,height,weight,activityLevel);
+            dietComponent.addDiet(diet);
+        }
+    }
+
 }
